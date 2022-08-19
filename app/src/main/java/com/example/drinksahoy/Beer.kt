2083175608ffc404955e6a52c18781e77c6ef76f
AhmadAziz0090 <72610079+AhmadAziz0090@users.noreply.github.com>
2022-08-19 22:05:44 +0100

    Create Beer.kt
    Created a data class, but could be implemented better

---
+++ app/src/main/java/com/example/drinksahoy/Beer.kt
@@ -0,0 +1,15 @@
+package com.example.drinksahoy
+
+data class Beer (
+    val image_url: String?,
+    val name: String?,
+    val strength: Int?,
+    val tagline: String?,
+    val description: String?,
+    val food_pair: String?
+){
+    override fun toString(): String {
+        return super.toString()
+        return "$image_url,$name,$strength,$tagline,$description,$food_pair"
+    }
+}
