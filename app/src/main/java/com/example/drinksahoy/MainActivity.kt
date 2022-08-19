5447f265435e1c606fe575819586e2ecb1f66a3f
AhmadAziz0090 <72610079+AhmadAziz0090@users.noreply.github.com>
2022-08-19 11:44:14 +0100

    Update to fix sort issues connecting with Punk Api
    - URL name was incorrect.
    - redid the Ion and Picasso call to punk api

--- app/src/main/java/com/example/drinksahoy/MainActivity.kt
+++ app/src/main/java/com/example/drinksahoy/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.drinksahoy
 
+import android.media.Image
 import android.os.Bundle
 import android.util.JsonReader
 import android.util.Log
@@ -19,27 +20,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_menu)
 
+        //button press connects android to punk api to fetch a random beer data
         val nextBeerBtn = findViewById<Button>(R.id.button)
         nextBeerBtn.setOnClickListener {
-                Ion.with(this)
-                    .load("https://api.punkapi.com/v2/beers/random")
-                    .asString()
-                    .setCallback {ex, result ->
-                        processPic(result)
-
-                    }
+            Ion.with(this)
+                .load("https://api.punkapi.com/v2/beers/random")
+                .asString()
+                .setCallback { e, result -> processBeer(result) }
             }
+
         }
-    private fun processPic(beerData: String){
+
+    //Processes the data fetched from punk api and displays it.
+    private fun processBeer(beerData: String) {
         val beerJson = JSONArray(beerData)
-        // returns the image url of the first item in list of beerJson.
         val url = beerJson
             .getJSONObject(0)
-            .getString("url")
+            .getString("image_url")
+
         val imgView = findViewById<ImageView>(R.id.imageView)
+
         Picasso
             .get()
             .load(url)
             .into(imgView)
     }
 }
+
+
