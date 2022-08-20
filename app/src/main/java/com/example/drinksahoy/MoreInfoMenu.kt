6c07049841b6d7f15252e527cfa7f9f6904f832a
AhmadAziz0090 <72610079+AhmadAziz0090@users.noreply.github.com>
2022-08-20 22:04:00 +0100

    Second activity displays more info on Beer

--- app/src/main/java/com/example/drinksahoy/MoreInfoMenu.kt
+++ app/src/main/java/com/example/drinksahoy/MoreInfoMenu.kt
@@ -2,8 +2,12 @@ package com.example.drinksahoy
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
 import java.io.Serializable
 
 class MoreInfoMenu : AppCompatActivity() {
@@ -15,10 +19,45 @@ class MoreInfoMenu : AppCompatActivity() {
         setContentView(R.layout.more_info_menu)
 
         val backBtn = findViewById<Button>(R.id.backBtn)
-        backBtn.setOnClickListener{
-            val intent = Intent(this,MainActivity::class.java)
-            intent.putExtra("beerInfo",currentBeer as Serializable)
+        backBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("beerInfo", currentBeer as Serializable)
             startActivity(intent)
         }
+
+        currentBeer = intent.extras!!.get("beerInfo") as Beer
+        fullBearInfo()
+
+    }
+    private fun fullBearInfo(){
+
+        //Display beer image using Picasso.
+        val imgView = findViewById<ImageView>(R.id.imageView)
+        Picasso
+            .get()
+            .load(currentBeer.imageUrl)
+            .into(imgView)
+
+        //Display beer name
+        val nameView = findViewById<TextView>(R.id.name)
+        nameView.text = "Name: ${currentBeer.name}"
+
+        //Display the strength of beer
+        //TODO Make sure beer displays strong icon when above 5% abv
+        val strengthView = findViewById<TextView>(R.id.strength)
+        strengthView.text = "Strength: ${currentBeer.strength}"
+
+        //Display the tagline of beer.
+        val taglineView = findViewById<TextView>(R.id.tagline)
+        taglineView.text = "Tagline: ${currentBeer.tagline}"
+
+        //Display the description of beer
+        val descriptionView = findViewById<TextView>(R.id.description)
+        descriptionView.text = "Description: ${currentBeer.description}"
+
+        //Display the foods that pair with beer
+        val foodPairView = findViewById<TextView>(R.id.food_pair)
+        foodPairView.text = "Food pairing: ${currentBeer.foodPair}"
+
     }
 }
