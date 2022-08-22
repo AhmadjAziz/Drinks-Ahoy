78b87d0d5c50d6d609d838e9771fda7ef8213413
AhmadAziz0090 <72610079+AhmadAziz0090@users.noreply.github.com>
2022-08-22 22:22:25 +0100

    Progressing on download functionality

---
+++ app/src/main/java/com/example/drinksahoy/DownloadsActivity.kt
@@ -0,0 +1,20 @@
+package com.example.drinksahoy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DownloadsActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.downloads_menu)
+
+    }
+
+    private fun populateList(): ArrayList<Beer> {
+        val list = ArrayList<Beer>()
+        //val
+        return list
+
+
+    }
+}
